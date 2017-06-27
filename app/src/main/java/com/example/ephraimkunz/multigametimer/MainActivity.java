e0ffbd44c5900375d35c26116f25df57d7f6ae94
Ephraim Kunz <ephraimkunz@me.com>
2017-06-27 07:53:15 -0700

    Add basic layout for home page, stub out onClickListeners

--- app/src/main/java/com/example/ephraimkunz/multigametimer/MainActivity.java
+++ app/src/main/java/com/example/ephraimkunz/multigametimer/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ephraimkunz.multigametimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createGame = (Button)findViewById(R.id.createGameButton);
+        createGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button joinGame = (Button)findViewById(R.id.joinGameButton);
+        joinGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
